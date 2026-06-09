fb83a4c0d96ff5b5ac8576904013c1d6d4ac6863
Adhithyan2004 <adhibanugopan@gmail.com>
2026-06-09 13:31:41 +0530

    addded types for dynamic data and a helper for formatting time

--- client/src/app/types/types.ts
+++ client/src/app/types/types.ts
@@ -4,3 +4,21 @@ export type ApplicationStatsData = {
   successRate: number;
   rejectionRate: number;
 };
+
+export type ApplicationRecordsData = {
+  totalApplications: number;
+  interviews: number;
+  offers: number;
+  rejected: number;
+  ghosted: number;
+};
+
+export type RecentActivityItems = {
+  id: string;
+  status: string;
+  changedAt: string;
+  application: {
+    companyName: string;
+    role: string;
+  };
+};
