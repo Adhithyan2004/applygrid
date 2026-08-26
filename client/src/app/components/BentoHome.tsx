747bc2f4737b12185245f065a47cbb94d7bfa39d
Adhithyan2004 <adhibanugopan@gmail.com>
2026-08-26 15:53:06 +0530

    Added favicon and added feedback in beto if no application available to display

--- client/src/app/components/BentoHome.tsx
+++ client/src/app/components/BentoHome.tsx
@@ -6,9 +6,12 @@ import { ApplicationStreak } from "./BentoComponents/ApplicationStreak";
 import { RecentActivity } from "./BentoComponents/RecentActivity";
 import { RecentApplications } from "./BentoComponents/RecentApplications";
 import { useDashboard } from "../hooks/useDashboard";
+import { useState } from "react";
+import { AddApplicationModal } from "./AddApplicationModal";
 
 const BentoHome = () => {
   const { data, isLoading } = useDashboard();
+  const [isModalOpen, setIsModalOpen] = useState(false);
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -41,14 +44,51 @@ const BentoHome = () => {
         <ApplicationStreak streak={data.streak} bestStreak={data.bestStreak} />
       </div>
       <div className="bg-white shadow-card col-span-6 row-span-6 rounded-xl py-5.5 px-8.25">
-        <RecentActivity
-          activities={data.recentActivity}
-          topRoles={data.topRoles}
-        />
+        {data.recentActivity.length > 0 ? (
+          <RecentActivity
+            activities={data.recentActivity}
+            topRoles={data.topRoles}
+          />
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center text-center">
+            <h2 className="text-2xl font-sora text-primary font-semibold">
+              No activity yet
+            </h2>
+
+            <p className="mt-1 text-sm text-gray-500">
+              Your recent application activity will appear here.
+            </p>
+          </div>
+        )}
       </div>
       <div className="bg-white shadow-card col-span-6 row-span-6 rounded-xl py-5.5 px-8.25">
-        <RecentApplications applications={data.recentApplications} />
+        {data.recentApplications.length > 0 ? (
+          <RecentApplications applications={data.recentApplications} />
+        ) : (
+          <div className="flex h-full flex-col items-center justify-center text-center">
+            <h2 className="text-xl font-semibold text-primary">
+              No applications to display, yet
+            </h2>
+
+            <p className="mt-2 text-sm text-gray-500">
+              Start tracking your job applications to see them here.
+            </p>
+
+            <button
+              className="px-5 py-2.5 font-sora text-primary font-semibold cursor-pointer"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Add Application
+            </button>
+          </div>
+        )}
       </div>
+      <AddApplicationModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        application={null}
+        mode="create"
+      />
     </div>
   );
 };
